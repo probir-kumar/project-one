fa933ac19e9db8e749b09acaf0a8675d3625ef3a
probir-kumar <probirkumarray2019@gmail.com>
2021-06-03 21:42:24 +0600

    some js code are add this project

---
+++ script.js
@@ -0,0 +1,7 @@
+// JS file
+
+document.querySelector(".btn").addEventListener("click", function () {
+  document.querySelector("h1").style.fontSize = "80px";
+  document.querySelector("h1").style.textTransform = "uppercase";
+  document.querySelector("h1").style.color = "magenta";
+});
