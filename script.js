1c117c0d920b261d10992a1720fbde9f72428f89
probir-kumar <85109138+probir-kumar@users.noreply.github.com>
2021-06-04 14:06:21 +0600

    little style and script updated

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 document.querySelector(".btn").addEventListener("click", function () {
   document.querySelector("h1").style.fontSize = "80px";
   document.querySelector("h1").style.textTransform = "uppercase";
-  document.querySelector("h1").style.color = "magenta";
+  document.querySelector("h1").style.color = "#fff";
 });
